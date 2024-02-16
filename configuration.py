4111a514c6170c2bf36597c8298b86704882c449
germanscheh <germanscheh@yandex.ru>
2024-02-16 17:09:14 +0300

    Добавил

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://836b8ff2-a362-4b10-a479-8413d4ff5ef4.serverhub.praktikum-services.ru"
+CREATE_COURIER = "/api/v1/courier"
+CREATE_ORDER = "/api/v1/orders"
+ORDER_INFORMATION = "/api/v1/orders/track?t="
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Чехонадских",
+    "lastName": "Герман",
+    "address": "Perova 12",
+    "metroStation": 4,
+    "phone": "+7 800 555 35 35",
+    "rentTime": 3,
+    "deliveryDate": "2023-18-02",
+    "comment": "Entrance 1",
+    "color": [
+        "BLACK"
+    ]
+}
